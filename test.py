4feb366a71994a4b70ceb0b97afcf1d076a91045
mattiaBrugna91 <124158814+mattiaBrugna91@users.noreply.github.com>
2023-06-22 16:45:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,3 @@ import numpy as np
 lista_random = np.random.rand(10)
 
 print(lista_random)
-
-# COMANDI UNIX operazioni da terminale per muovarmi sulle cartelle: 
-# ls = lista cartelle e file presenti 
-# cd <nome cartella> = cambio cartella 
-# pwd = ci spiega dove siamo <path> 
-# python test.py = testa codice 
-# git init = creo repository git
